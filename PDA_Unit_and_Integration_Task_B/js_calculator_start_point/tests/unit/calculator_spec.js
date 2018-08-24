2f204aa6be9afc83fbcb3f28b772a4c7bdbcb77d
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-24 14:57:08 +0100

    calculator passes contacenation of multiple number button clicks integration test

--- PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -12,31 +12,33 @@ describe('calculator', function () {
   });
 
   it('should be able to add 1 to 4 and get 5', function () {
-    calculator.numberClick(4);
-    calculator.operatorClick('=');
+    calculator.previousTotal = 4;
     calculator.add(1);
     assert.equal(calculator.runningTotal, 5);
   });
 
   it('should be able to subtract 4 from 7 and get 3', function () {
-    calculator.numberClick(7);
-    calculator.operatorClick('=');
+    calculator.previousTotal = 7;
     calculator.subtract(4);
     assert.equal(calculator.runningTotal, 3);
   });
 
   it('should be able to multiply 3 by 5 and get 15', function () {
-    calculator.numberClick(3);
-    calculator.operatorClick('=');
+    calculator.previousTotal = 3;
     calculator.multiply(5);
     assert.equal(calculator.runningTotal, 15);
   });
 
   it('should be able to divide 21 by 7 and get 3', function () {
-    calculator.numberClick(21);
-    calculator.operatorClick('=');
+    calculator.previousTotal = 21;
     calculator.divide(7);
     assert.equal(calculator.runningTotal, 3);
   });
+
+  it('can concatenate multiple number button clicks', function () {
+    calculator.numberClick(21);
+    assert.equal(calculator.runningTotal, 21);
+  });
+
 
 });
