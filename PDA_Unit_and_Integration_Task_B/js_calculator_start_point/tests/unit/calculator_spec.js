6a7dd302fb5ecfd0c87ba5aeca19ddb3ea3abd5d
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-24 11:13:22 +0100

    calculator passes addition unit test

--- PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,6 +9,15 @@ describe('calculator', function () {
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
-  })
+  });
+
+  it('should be able to add 1 to 4 and get 5', function () {
+    calculator.numberClick(4);
+    calculator.operatorClick('+');
+    calculator.numberClick(1);
+    calculator.operatorClick('=');
+    const result = calculator.previousTotal;
+    assert.equal(result, 5);
+  });
 
 });
