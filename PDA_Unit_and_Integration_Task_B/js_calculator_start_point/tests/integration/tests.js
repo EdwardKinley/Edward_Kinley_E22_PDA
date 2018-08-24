330091d77f1d5a3f6f0ef265904bdabb6e6917d0
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-24 16:00:08 +0100

    calculator passes division by zero (returning 'Not a Number') UI integration test

--- PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ PDA_Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -63,4 +63,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-2500000');
   });
 
+  it('should return NaN in response to division by zero', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Not a Number');
+  });
+
 });
