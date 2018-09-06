433438402bbabd30161c8d33be704a1b66e19f24
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-09-06 23:05:37 +0100

    I.T.5 amends array function

--- pda.rb
+++ pda.rb
@@ -1,15 +1,23 @@
-items = ["ark", "bottle", 1, 5, "everyone", "dragon", true, "cauliflower", 3.14159, [1, 2, 3], {"key" => "value"}]
-# puts "Here is an array of items of various kinds:"
-# p items
-# puts
+numbers_array = [1, 2, 3, 4, 5, 6]
 
-items_strings = []
-items.each{|item| items_strings.push(item) if item.class == String}
-# puts "Here is an array containing the results of searching for items of String class:"
-# p items_strings
-# puts
+def square(numbers)
+  numbers.each{ |number| p number ** 2 }
+end
 
-puts "Here is an array containing the String class items sorted into alphabetical order:"
-items_strings_alphabetical = items_strings.sort
-p items_strings_alphabetical
-puts
+square(numbers_array)
+
+# items = ["ark", "bottle", 1, 5, "everyone", "dragon", true, "cauliflower", 3.14159, [1, 2, 3], {"key" => "value"}]
+# # puts "Here is an array of items of various kinds:"
+# # p items
+# # puts
+#
+# items_strings = []
+# items.each{|item| items_strings.push(item) if item.class == String}
+# # puts "Here is an array containing the results of searching for items of String class:"
+# # p items_strings
+# # puts
+#
+# puts "Here is an array containing the String class items sorted into alphabetical order:"
+# items_strings_alphabetical = items_strings.sort
+# p items_strings_alphabetical
+# puts
