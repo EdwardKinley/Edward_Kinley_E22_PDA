3e4c54b6fd793ceae72d78ec464dddf488c33332
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-09-06 23:19:36 +0100

    I.T.3 amends search function

--- pda.rb
+++ pda.rb
@@ -1,10 +1,31 @@
-pets_hash = {"axolotl" => "Aristotl", "budgie" => "Bernadette", "cat" => "Cyril", "dog" => "Dougie"}
+items = ["ark", "bottle", 1, 5, "everyone", "dragon", true, "cauliflower", 3.14159, [1, 2, 3], {"key" => "value"}]
+puts "Here is an array of items of various kinds:"
+p items
+puts
 
-def pets_names(pets)
-  pets.each { |animal, name| puts "The #{animal} is named #{name}." }
+def get_strings(items)
+  items_strings = []
+  items.each{|item| items_strings.push(item) if item.class == String}
+  return items_strings
 end
+puts "Here is an array containing the results of searching for items of String class:"
+p get_strings(items)
+puts
 
-pets_names(pets_hash)
+# puts "Here is an array containing the String class items sorted into alphabetical order:"
+# items_strings_alphabetical = items_strings.sort
+# p items_strings_alphabetical
+# puts
+
+
+
+# pets_hash = {"axolotl" => "Aristotl", "budgie" => "Bernadette", "cat" => "Cyril", "dog" => "Dougie"}
+#
+# def pets_names(pets)
+#   pets.each { |animal, name| puts "The #{animal} is named #{name}." }
+# end
+#
+# pets_names(pets_hash)
 
 
 # numbers_array = [1, 2, 3, 4, 5, 6]
@@ -14,19 +35,3 @@ pets_names(pets_hash)
 # end
 #
 # square(numbers_array)
-
-# items = ["ark", "bottle", 1, 5, "everyone", "dragon", true, "cauliflower", 3.14159, [1, 2, 3], {"key" => "value"}]
-# # puts "Here is an array of items of various kinds:"
-# # p items
-# # puts
-#
-# items_strings = []
-# items.each{|item| items_strings.push(item) if item.class == String}
-# # puts "Here is an array containing the results of searching for items of String class:"
-# # p items_strings
-# # puts
-#
-# puts "Here is an array containing the String class items sorted into alphabetical order:"
-# items_strings_alphabetical = items_strings.sort
-# p items_strings_alphabetical
-# puts
